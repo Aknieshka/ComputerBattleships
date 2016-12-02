1eae5983a43686d527332bcf560fdc2e0cbd66ea
WilhelmOlejnik <wilhelmolejnik@gmail.com>
2016-12-03 00:45:10 +0100

    added board generating

--- Ship.py
+++ Ship.py
@@ -1,13 +1,19 @@
 class Ship:
-    id = None
-    masts = None
-    coordinates = []
 
-    def __init__(self):
-        print("Ship created")
+    def __init__(self, id, masts, direction, upperLeftCoords):
+        self.id = id
+        self.masts = masts
+        self.coordinates = []
+        self.CreateCoordinates(masts, direction, upperLeftCoords)
 
     def IsShotDown(self):
         print("Check if ship is shot down")
 
-    def CreateCoordinates(self, masts, direction):
-        print("Create here coordinates depending on masts and vertical/horizontal direction")
+    def CreateCoordinates(self, masts, direction, upperLeftCoords):
+        upperLeftX = upperLeftCoords[0]
+        upperLeftY = upperLeftCoords[1]
+        for i in range(masts):
+            if(direction == "horizontal"):
+                self.coordinates.append([upperLeftX + i, upperLeftY])
+            else:
+                self.coordinates.append([upperLeftX, upperLeftY + i])
